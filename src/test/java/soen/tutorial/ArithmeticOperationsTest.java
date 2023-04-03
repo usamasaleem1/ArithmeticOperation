043371617732636374d71486e73b6a4692396b28
Usama Saleem <usama.saleem@hotmail.com>
2023-04-03 16:16:26 -0400

    fail
    fail

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -3,29 +3,29 @@ package soen.tutorial;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class ArithmeticOperationsTest {
 
-
 	@Test
-	public void testAdd()
-	{
+	public void testAdd() {
 
 		ArithmeticOperations operations = new ArithmeticOperations();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		assertEquals(expected, actual);
+		// assertEquals(expected, actual);
+		fail("must fail");
 
 	}
-//     @Test
-//     public void testSub()
-//     {
+	// @Test
+	// public void testSub()
+	// {
 
-//         ArithmeticOperations operations = new ArithmeticOperations();
-//         Integer actual = operations.addOrSub(6, 2);
-//         Integer expected = 4;
-//         assertEquals(expected, actual);
+	// ArithmeticOperations operations = new ArithmeticOperations();
+	// Integer actual = operations.addOrSub(6, 2);
+	// Integer expected = 4;
+	// assertEquals(expected, actual);
 
-//     }
+	// }
 
 }
